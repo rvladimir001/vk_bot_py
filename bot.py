01911f26fd6322b859f7db2a21023960aadf95cb
Владимир <rvladimir001@gmail.com>
2020-05-10 22:25:31 +0300

    добавлена обработка интенетов и сценариев

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
 import vk_api
-from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType, VkBotMessageEvent
 import random
 import logging
+import handlers
 
 try:
     import settings
@@ -25,6 +27,15 @@ def configure_logging():
     log.setLevel(logging.DEBUG)
 
 
+class UserState:
+    """Состояние пользователя внтури сценария"""
+
+    def __init__(self, scenario_name, step_name, context=None):
+        self.scenario_name = scenario_name
+        self.step_name = step_name
+        self.context = context or {}
+
+
 class Bot:
     """
     Echo bot for vk
@@ -42,6 +53,7 @@ class Bot:
         self.vk = vk_api.VkApi(token=token)
         self.long_poller = VkBotLongPoll(self.vk, self.group_id)
         self.api = self.vk.get_api()
+        self.user_states = dict()  # user_id -> UserState
 
     def run(self):
         """запуск бота"""
@@ -57,19 +69,58 @@ class Bot:
         :param event:VkBotEventMessage object
         :return None
         """
-        if event.type == VkBotEventType.MESSAGE_NEW:
-            log.info("Отправка сообщания")
-            mes = event.object.message['text']
-            peer_id = event.object.message['peer_id']
-            log.info(f'Получено сообщение: "{mes}"')
-            mes = f'Ваш запрос: "{mes}"'
-            self.api.messages.send(
-                message=mes,
-                random_id=random.randint(0, 2 ** 5),
-                peer_id=peer_id
-            )
+        if event.type != VkBotEventType.MESSAGE_NEW:
+            log.info("Мы пока не умеем обрабатывать события такого типа %s", event.type)
+            return
+        user_id = event.object['message']['peer_id']
+        text = event.object['message']['text']
+        if user_id in self.user_states:
+            text_to_send = self.continue_scenario(user_id, text)
         else:
-            log.info('пока не обрабатываются события %s', event.type)
+            # search intent
+            for intent in settings.INTENTS:
+                log.debug(f'User gets intent {intent}')
+                if any(token in text for token in intent['tokens']):
+                    # run intent
+                    if intent['answer']:
+                        text_to_send = intent['answer']
+                    else:
+                        text_to_send = self.start_scenario(user_id, intent['scenario'])
+                    break
+            else:
+                text_to_send = settings.DEFAULT_ANSWER
+        self.api.messages.send(
+            message=text_to_send,
+            random_id=random.randint(0, 2 ** 5),
+            peer_id=user_id
+        )
+
+    def continue_scenario(self, user_id, text):
+        # continue scenario
+        state = self.user_states[user_id]
+        steps = settings.SCENARIOS[state.scenario_name]['steps']
+        step = steps[state.step_name]
+        handler = getattr(handlers, step['handler'])
+        if handler(text=text, context=state.context):
+            # next step
+            next_step = steps[step['next_step']]
+            text_to_send = next_step['text'].format(**state.context)
+            if next_step['next_step']:
+                state.step_name = step['next_step']
+            else:
+                log.info('Зарегистрирован {name}{email}'.format(**state.context))
+                self.user_states.pop(user_id)
+        else:
+            text_to_send = step['failure_text'].format(**state.context)
+        return text_to_send
+
+    def start_scenario(self, user_id, scenario_name):
+        scenario = settings.SCENARIOS[scenario_name]
+        first_step = scenario['first_step']
+        step = scenario['steps'][first_step]
+        text_to_send = step['text']
+        self.user_states[user_id] = UserState(scenario_name=scenario_name, step_name=first_step)
+        return text_to_send
 
 
 if __name__ == "__main__":
